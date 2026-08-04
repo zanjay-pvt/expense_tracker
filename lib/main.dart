8217adc0f501cc4a17144cb04ed6d79c6a64be76
zanjay-pvt <zanjay.pvt@gmail.com>
2026-08-04 22:30:37 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:expense_tracker/data/expense_data.dart';
 import 'package:expense_tracker/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
-  //initialize hive
   await Hive.initFlutter();
-  //open a hive app
   await Hive.openBox("Expense_database");
   runApp(MyApp());
 }
